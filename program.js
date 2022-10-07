a18483d341fb93ec5abc832b7a36ba0be822688c
valerie2405 <valerie.arobert@gmail.com>
2022-10-06 22:10:02 -0400

    all code

--- program.js
+++ program.js
@@ -6,11 +6,12 @@ function Addpoint(){
     //turn to text
     let xvalsText = xvals.options[xvals.selectedIndex].text;
     let yvalsText = yvals.options[yvals.selectedIndex].text;
-
-    let newPoint = "<circle cx="+ xvalsText*40 + "cy="+ 400-40*yvalsText + "r=10" ;
-    console.log('point printed') //not showing point on frame -- point might be showing up behind frame ??
+    let x_coord = xvalsText * 50;
+    let y_coord = 500 - (yvalsText * 50);
+    let circ_id = `id="(${xvalsText}, ${yvalsText})"`
+    let newPoint = `<circle ${circ_id} cx="${x_coord}" cy="${y_coord}" r="10"/>`;
     document.getElementById("frame").innerHTML += newPoint;
-
+    // adds the functionality to all the points 
     addFunctionality();
 
 }
@@ -18,14 +19,15 @@ function Addpoint(){
 function clickedPoint(point){
     let ClickedPoint= document.getElementById(point.target.id);
     let PointCoordinate = document.getElementById("clicked_coordinates");
-
+    // if point already selected, remove, else select it
     if (ClickedPoint.classList.contains("selected")) {
 		ClickedPoint.classList.remove("selected");
+        PointCoordinate.innerHTML = " ";
 	}
 	else {
 		ClickedPoint.classList.add("selected");
+        PointCoordinate.innerHTML = "Last point clicked:" + point.target.id;
 	}
-	PointCoordinate.innerHTML = "Last point clicked:" + point.target.id;
 }
 //itterate through all circles to add functionality to points
 function addFunctionality() {
@@ -33,8 +35,6 @@ function addFunctionality() {
 
     for (const element of allPoints) {
     element.addEventListener('click', clickedPoint)
+    }
 }
-}
-//add functionality
 addFunctionality();
-
